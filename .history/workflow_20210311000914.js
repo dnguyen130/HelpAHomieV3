1399eb601f741fdef7d9e33f93c7b5392dfc7c31
wilyyy <alvarez_william@outlook.com>
2021-03-11 01:18:51 -0800

    2nd commit

---
+++ .history/workflow_20210311000914.js
@@ -0,0 +1,19 @@
+//home page
+document.querySelector("#door-screen1").innerHTML += TriangleButtonUI("door-screen1");
+
+// door page
+document.querySelector("#door-screen1").innerHTML += TriangleButtonUI("door-screen1");
+document.querySelector("#door-screen1").innerHTML += DoorUI()
+document.querySelector("#door-screen1").innerHTML += TriangleButtonUI("door-screen2", "images/Circle-Triangle-UpsideDown.png");
+
+document.querySelector("#door-screen2").innerHTML += TriangleButtonUI("door-screen1");
+document.querySelector("#door-screen2").innerHTML += DoorUI("STACY");
+document.querySelector("#door-screen2").innerHTML += TriangleButtonUI("door-screen3", "images/Circle-Triangle-UpsideDown.png");
+
+document.querySelector("#door-screen3").innerHTML += TriangleButtonUI("door-screen2");
+document.querySelector("#door-screen3").innerHTML += DoorUI("GREG");
+document.querySelector("#door-screen3").innerHTML += TriangleButtonUI("door-screen1", "images/Circle-Triangle-UpsideDown.png");
+
+
+
+
